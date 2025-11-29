dbb178ea46a29bfff7d4f2b3c8546d22fa70b8fb
techemmydev <gabrielemmanuelmary12345@gmail.com>
2025-11-29 14:52:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <h1 class="text-3xl font-bold underline bg-amber-900">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
